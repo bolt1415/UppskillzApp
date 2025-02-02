f94e7e41acf53070782797ad9bdcc3090a7f899e
bolt1415 <leapf975@gmail.com>
2025-02-02 12:28:37 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Heart } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="w-full p-4 text-center text-sm bg-gradient-to-r from-[#6D28D9] to-[#60A5FA]">
+    <footer className="w-full p-4 text-center text-sm bg-gradient-to-r from-[#6767EA] to-[#60A5FA]">
       <div className="opacity-30 select-none hover:opacity-70 transition-opacity duration-300" style={{ color: 'black' }}>
         <span className="font-medium tracking-wider">
           developed with <Heart className="inline h-4 w-4 text-red-500/70 mx-1" /> by{" "}
